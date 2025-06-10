4952f54cdd33e00a000333f28d49e5e8ae67c833
Tushar9071 <tusharrajpara00@gmail.com>
2025-06-10 18:15:00 +0530

    abcd

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Limit = require('express-rate-limit');
 // Rate limiter configuration
 const limit = Limit({
   windowMs: 1000 * 60,
-  max: 2500,
+  max: 100,
   message: "too many requests"
 });
 
@@ -50,20 +50,7 @@ if (cluster.isMaster) {
   // CPU Usage
   const startUsage = process.cpuUsage();
 
-  // RAM and CPU Usage monitoring
-//   setInterval(() => {
-//     const memory = process.memoryUsage();
-//     console.clear();
-//     const usage = process.cpuUsage(startUsage);
-//     const user = usage.user / 1000000; // Convert microseconds to seconds
-//     const system = usage.system / 1000000;
-//     console.log(`Worker ${process.pid} - CPU Usage: User ${user.toFixed(2)}s, System ${system.toFixed(2)}s`);
-//     console.log(`Worker ${process.pid} - RAM Usage:`);
-//     console.log(`  RSS: ${(memory.rss / 1024 / 1024).toFixed(2)} MB`);
-//     console.log(`  Heap Total: ${(memory.heapTotal / 1024 / 1024).toFixed(2)} MB`);
-//     console.log(`  Heap Used: ${(memory.heapUsed / 1024 / 1024).toFixed(2)} MB`);
-//     console.log(`  External: ${(memory.external / 1024 / 1024).toFixed(2)} MB`);
-//   }, 1000);
+
 
   // Apply rate limiter
   app.use(limit);
